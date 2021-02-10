eed9a93120f5acb664d15562c6f625eafb940549
Krishnaswamy Subramanian <jskswamy@gmail.com>
2021-02-10 07:52:52 +0530

    Update k3d terraform modules

--- kubernetes/cluster/k3d.tf
+++ kubernetes/cluster/k3d.tf
@@ -1,7 +1,9 @@
 module "k3d_demo_cluster" {
-  source       = "github.com/infra-as-code-workshop/terraform-modules/k3d"
-  cluster_name = "opa"
-  node_count   = 3
+  source             = "github.com/infra-as-code-workshop/terraform-modules/k3d"
+  cluster_name       = "opa"
+  node_count         = 3
+  kubernetes_version = "1.20.2"
+  registry_port      = 5002
 }
 
 output "kubeconfig" {
